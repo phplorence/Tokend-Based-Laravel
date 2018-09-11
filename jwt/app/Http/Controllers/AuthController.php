4175d1fc4ed291a6125fe518bc9bcd6ad6b21bc0
lorence <lorence@enclave.vn>
2018-09-11 09:25:45 +0700

    Test

--- jwt/app/Http/Controllers/AuthController.php
+++ jwt/app/Http/Controllers/AuthController.php
@@ -1,14 +1,13 @@
-
 <?php
 
 namespace App\Http\Controllers;
+
 use App\User;
-use DB;
-use Hash;
 use Illuminate\Http\Request;
-use JWTAuth;
-use Mail;
-use Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -29,8 +28,8 @@ class AuthController extends Controller
 
         $validator = Validator::make($credentials, $rules);
 
-        if($validator->fails()) {
-            return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'error' => $validator->messages()]);
         }
 
         $name = $request->name;
@@ -38,17 +37,127 @@ class AuthController extends Controller
         $password = $request->password;
 
         $user = User::create(['name' => $name, 'email' => $email, 'password' => Hash::make($password)]);
-        $verification_code = str_random(30); //Generate verification code
+        $verification_code = str_random(30);
 
-        DB::table('user_verifications')->insert(['user_id'=>$user->id,'token'=>$verification_code]);
+        DB::table('user_verifications')->insert(['user_id' => $user->id, 'token' => $verification_code]);
 
         $subject = "Please verify your email address.";
         Mail::send('email.verify', ['name' => $name, 'verification_code' => $verification_code],
-            function($mail) use ($email, $name, $subject){
+            function ($mail) use ($email, $name, $subject) {
                 $mail->from(getenv('FROM_EMAIL_ADDRESS'), "From User/Company Name Goes Here");
                 $mail->to($email, $name);
                 $mail->subject($subject);
             });
-        return response()->json(['success'=> true, 'message'=> 'Thanks for signing up! Please check your email to complete your registration.']);
+        return response()->json(['success' => true, 'message' => 'Thanks for signing up! Please check your email to complete your registration.']);
     }
+
+    /**
+     * API Verify User
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function verifyUser($verification_code)
+    {
+        $check = DB::table('user_verifications')->where('token', $verification_code)->first();
+        if (!is_null($check)) {
+            $user = User::find($check->user_id);
+            if ($user->is_verified == 1) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Account already verified..'
+                ]);
+            }
+            $user->update(['is_verified' => 1]);
+            DB::table('user_verifications')->where('token', $verification_code)->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'You have successfully verified your email address.'
+            ]);
+        }
+        return response()->json(['success' => false, 'error' => "Verification code is invalid."]);
+    }
+
+    /**
+     * API Login, on success return JWT Auth token
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+        $input = $request->only('email', 'password');
+        $validator = Validator::make($input, $rules);
+        if($validator->fails()) {
+            $error = $validator->messages()->toJson();
+            return response()->json(['success'=> false, 'error'=> $error]);
+        }
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+            'is_verified' => 1
+        ];
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['success' => false, 'error' => 'Invalid Credentials. Please make sure you entered the right information and you have verified your email address.'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'could_not_create_token'], 500);
+        }
+        // all good so return the token
+        return response()->json(['success' => true, 'data'=> [ 'token' => $token ]]);
+    }
+
+    /**
+     * Log out
+     * Invalidate the token, so user cannot use it anymore
+     * They have to relogin to get a new token
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request) {
+        $this->validate($request, ['token' => 'required']);
+        try {
+            JWTAuth::invalidate($request->input('token'));
+            return response()->json(['success' => true]);
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'Failed to logout, please try again.'], 500);
+        }
+    }
+
+    /**
+     * API Recover Password
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function recover(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            $error_message = "Your email address was not found.";
+            return response()->json(['success' => false, 'error' => ['email'=> $error_message]], 401);
+        }
+        try {
+            Password::sendResetLink($request->only('email'), function (Message $message) {
+                $message->subject('Your Password Reset Link');
+            });
+        } catch (\Exception $e) {
+            //Return with error
+            $error_message = $e->getMessage();
+            return response()->json(['success' => false, 'error' => $error_message], 401);
+        }
+        return response()->json([
+            'success' => true, 'data'=> ['msg'=> 'A reset email has been sent! Please check your email.']
+        ]);
+    }
+
 }
