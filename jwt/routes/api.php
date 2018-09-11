9f132b2a081c8b703458f7497eadc67f0233d9ad
lorence <lorence@enclave.vn>
2018-09-11 09:56:17 +0700

    Done

--- jwt/routes/api.php
+++ jwt/routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', 'AuthController@register')->name('api.login');
+Route::post('register', 'AuthController@register')->name('api.register');
 Route::post('login', 'AuthController@login');
 Route::post('recover', 'AuthController@recover');
 Route::group(['middleware' => ['jwt.auth']], function() {
